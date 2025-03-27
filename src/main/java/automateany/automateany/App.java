fa981c8953c5fe361d92f63e7f9eb907f328dbb2
pratik <pratik@creditfair.in>
2025-03-27 14:43:53 +0530

    first initiate

---
+++ src/main/java/automateany/automateany/App.java
@@ -0,0 +1,10 @@
+package automateany.automateany;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
